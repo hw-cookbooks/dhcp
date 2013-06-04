f4d722e58159de1072e86d24490188524de84575
Michael F. Weinberg <michael@luckymike.com>
2013-06-04 14:19:13 -0700

    Use FileEdit for providers.

--- providers/subnet.rb
+++ providers/subnet.rb
@@ -5,23 +5,21 @@ action :add do
     cookbook "dhcp"
     source "subnet.conf.erb"
     variables(
-      :subnet => new_resource.subnet,
-      :netmask => new_resource.netmask,
-      :ranges => new_resource.ranges,
-      :options => new_resource.options,
-      :parameters => new_resource.parameters,
-      :groups => new_resource.groups
-    )
+              :subnet => new_resource.subnet,
+              :netmask => new_resource.netmask,
+              :ranges => new_resource.ranges,
+              :options => new_resource.options,
+              :parameters => new_resource.parameters,
+              :groups => new_resource.groups
+              )
     owner "root"
     group "root"
     mode 0644
+    file = Chef::Util::FileEdit.new("#{node[:dhcp][:config_dir]}/subnets.d/subnet_list.conf")
+    file.insert_line_if_no_match("#{filename};","include \"#{filename}\";")
+    file.write_file
     notifies :restart, resources(:service => node[:dhcp][:package]), :delayed
   end
-  #utils_line "include \"#{filename}\";" do
-  #  action :add
-  #  file "#{node[:dhcp][:config_dir]}/subnets.d/subnet_list.conf"
-  #  notifies :restart, resources(:service => node[:dhcp][:package]), :delayed
-  #end
 end
 
 action :remove do
@@ -34,10 +32,8 @@ action :remove do
     end
     new_resource.updated_by_last_action(true)
   end
-  #utils_line "include \"#{filename}\";" do
-  #  action :remove
-  #  file "#{node[:dhcp][:config_dir]}/subnets.d/subnet_list.conf"
-  #  notifies :restart, resources(:service => node[:dhcp][:package]), :delayed
-  #end
+  file = Chef::Util::FileEdit.new("#{node[:dhcp][:config_dir]}/subnets.d/subnet_list.conf")
+  file.search_file_delete_line("include \"#{filename}\";")
+  file.write_file
+  notifies :restart, resources(:service => node[:dhcp][:package]), :delayed
 end
-
